704067e16afed52fbbfdeef2cd2dda7032287067
Stepalom77 <stepalom22@gmail.com>
2022-10-07 17:15:31 -0500

    Project Structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports:[UsersModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class UsersModule {}
