e3de9511672eb0ef5fdcd1d0da4842587320c818
Stepalom77 <stepalom22@gmail.com>
2022-10-08 09:40:45 -0500

    update method added to User Entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Body, Get, Post, Patch, Delete, Param, Query } from '@nestjs/common';
 import { createUsersDto } from './dtos/create-users.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 @Controller('/auth')
 export class UsersController {
@@ -24,5 +25,10 @@ export class UsersController {
     @Delete('/delete/:id')
     delete(@Param('id') id:string){
         return this.usersService.deleteUser(parseInt(id))
+    }
+
+    @Patch('/update/:id')
+    update(@Param('id') id:string, @Body() body:UpdateUserDto){
+        return this.usersService.updateUser(parseInt(id), body)
     }
 }
