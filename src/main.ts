47dcddcd7cfd54d01fdef7d098b498aecfe435af
Stepalom77 <stepalom22@gmail.com>
2022-10-13 14:48:15 -0500

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Used Cars Pricing Calculator API')
+    .setDescription('The API of an app that calculates the price of used cars')
+    .setVersion('1.0')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
   await app.listen(3000, () => {
     console.log('Listening on port:3000')
