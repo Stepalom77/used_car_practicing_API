6c47423624c78ad6d9e0f33a4c210101e670b078
Stepalom77 <stepalom22@gmail.com>
2022-10-13 11:00:53 -0500

    Typeorm migrations setup added

---
+++ migrations/1665669488925-init-schema.js
@@ -0,0 +1,23 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class initSchema1665669488925 {
+    name = 'initSchema1665669488925'
+
+    async up(queryRunner) {
+        await queryRunner.query(`CREATE TABLE "users" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, "admin" boolean NOT NULL DEFAULT (1))`);
+        await queryRunner.query(`CREATE TABLE "reports" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "approved" boolean NOT NULL DEFAULT (0), "price" integer NOT NULL, "make" varchar NOT NULL, "model" varchar NOT NULL, "year" integer NOT NULL, "lng" integer NOT NULL, "lat" integer NOT NULL, "mileage" integer NOT NULL, "userId" integer)`);
+        await queryRunner.query(`CREATE TABLE "temporary_reports" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "approved" boolean NOT NULL DEFAULT (0), "price" integer NOT NULL, "make" varchar NOT NULL, "model" varchar NOT NULL, "year" integer NOT NULL, "lng" integer NOT NULL, "lat" integer NOT NULL, "mileage" integer NOT NULL, "userId" integer, CONSTRAINT "FK_bed415cd29716cd707e9cb3c09c" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`);
+        await queryRunner.query(`INSERT INTO "temporary_reports"("id", "approved", "price", "make", "model", "year", "lng", "lat", "mileage", "userId") SELECT "id", "approved", "price", "make", "model", "year", "lng", "lat", "mileage", "userId" FROM "reports"`);
+        await queryRunner.query(`DROP TABLE "reports"`);
+        await queryRunner.query(`ALTER TABLE "temporary_reports" RENAME TO "reports"`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "reports" RENAME TO "temporary_reports"`);
+        await queryRunner.query(`CREATE TABLE "reports" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "approved" boolean NOT NULL DEFAULT (0), "price" integer NOT NULL, "make" varchar NOT NULL, "model" varchar NOT NULL, "year" integer NOT NULL, "lng" integer NOT NULL, "lat" integer NOT NULL, "mileage" integer NOT NULL, "userId" integer)`);
+        await queryRunner.query(`INSERT INTO "reports"("id", "approved", "price", "make", "model", "year", "lng", "lat", "mileage", "userId") SELECT "id", "approved", "price", "make", "model", "year", "lng", "lat", "mileage", "userId" FROM "temporary_reports"`);
+        await queryRunner.query(`DROP TABLE "temporary_reports"`);
+        await queryRunner.query(`DROP TABLE "reports"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+    }
+}
